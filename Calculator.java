f38f904ee54ef2929e442870f4eefb1efbd83593
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-20 00:54:23 +0530

    creating default constructor

--- Calculator.java
+++ Calculator.java
@@ -2,3 +2,13 @@
 
 public class Calculator
 {
+    // initialize an array
+    int[] input_nums;
+
+    // default constructor (may throw InvalidInputException in input())
+    public Calculator() throws InvalidInputException {
+        // create a new Input object and add numbers to the array
+        Input in = new Input();
+        input_nums = in.input();
+    }
+
