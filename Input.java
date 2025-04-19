c6c1451de23a2b3b1dbd77fef41509f49ab9ed96
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-20 00:56:34 +0530

    create input

---
+++ Input.java
@@ -0,0 +1,12 @@
+// Input.java
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Input
+{
+    // method to take input from user
+    int[] input() throws InvalidInputException
+    {
+        Scanner scan = new Scanner(System.in);
+
